7e2ef7f1f838f37aa5185a0b3b1e719468e27bb3
ackerman20 <73731858+ackerman20@users.noreply.github.com>
2024-08-29 00:56:44 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -13,56 +13,27 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-function write_db() {
-    console.log("DEBUG: Write function");
+document.getElementById("read").addEventListener("click", function(){
     var db = getDatabase();
-    var create_db_table = ref(db, 'Y/' + '4710085247479/');
-    var user_name = document.getElementById("user_name").value;
-    var user_password =  document.getElementById("user_name_password").value;
-    if( user_name == '' || user_password == ''){
-        alert("Make sure, must be non-empty data is required!!!");
-        console.log("Make sure, must be non-empty data is required!!!");
-        throw "Make sure, must be non-empty data is required!!!";
-    }
-    set(ref(db, 'Y/' + '4710085247479/'), {
-      user_name: user_name,
-      user_name_password: user_password
-    }).then((res) => {
-        console.log();
-    })
-    .catch((err) => {
-        alert(err.message);
-        console.log(err.code);
-        console.log(err.message);
-    })
-}
-
-function read_db() {
-    var db = getDatabase();
-    var connect_db = ref(db, 'Y/' + '4710085247479/');
+    var retailInput = document.getElementById("retail");
+    var retailId = retailInput.value;
+    var connect_db = ref(db, 'Y/' + retailId + '/');
     var retrieve_data='';
     var responseText = document.getElementById("response");
     responseText.textContent = "";
+    retailInput.textContent = "";
     console.log("DEBUG: Read function");
     onValue(connect_db, (snapshot) => {
         retrieve_data = snapshot.val();
-        //console.log("user_name: " + retrieve_data.user_name);
-        //console.log("user_name_password: " + retrieve_data.user_name_password);
         call_loop_print(retrieve_data);
         var Text = retrieve_data.name + "擺在: " + retrieve_data.location;
         responseText.textContent = Text
-        })
+    })
     function call_loop_print(retrieve_data){
         for (var r=0;r<Object.entries(retrieve_data).length;r++){
             var key = Object.keys(retrieve_data)[r];
             var value = retrieve_data[key];
             console.log("Key_" + r + ': ' + key + " Value_:" + r + ': ' + value );
             }
- }
-}
-
-var write_data_to_firebase = document.getElementById("write_data_to_firebase");
-write_data_to_firebase.addEventListener('click', write_db);
-
-var read_data_from_firebase = document.getElementById("read_data_from_firebase");
-read_data_from_firebase.addEventListener('click', read_db);
+    }
+});
